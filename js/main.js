460098ec0a66190441351cd299e2f7e23fdb58fc
paul76546 <68160193+paul76546@users.noreply.github.com>
2020-12-22 08:33:16 +0300

    Upd

--- js/main.js
+++ js/main.js
@@ -132,6 +132,15 @@ allPosts: [
     date: '10.11.2020, 20:54:00',
     like: 45,
     comments: 12,
+  },
+  {
+    title: 'Заголовок поста3',
+    text: 'Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты. Языком что рот маленький реторический вершину текстов обеспечивает гор свой назад решила сбить маленькая дорогу жизни рукопись ему букв деревни предложения, ручеек залетают продолжил парадигматическая? Но языком сих пустился, запятой своего его снова решила меня вопроса моей своих пояс коварный, власти диких правилами напоивший они текстов ipsum первуюподпоясал? Лучше, щеке подпоясал приставка большого курсивных на берегу своего? Злых, составитель агентство что вопроса ведущими о решила одна алфавит!',
+    tags: ['свежее', 'новое', 'горячее', 'мое', 'случайность'],
+    author: 'maks@mail.com',
+    date: '11.11.2020, 20:54:00',
+    like: 15,
+    comments: 20,
   }
 
 ]
@@ -169,12 +178,60 @@ const toggleAuthDom = () => {
 const showAllPosts = () => {
   let postsHTML = '';
 
-  setPosts.allPosts.forEach((post, i, arr) => {
-    //console.log(post, i, arr);
+
+
+  setPosts.allPosts.forEach(({title, text, date}) => {
+
+
+
+    postsHTML += `
+    <section class="post">
+        <div class="post-body">
+          <h2 class="post-title">${title}</h2>
+          <p class="post-text">${text}</p>
+          <div class="tags">
+            <a href="#" class="tag">#свежее</a>
+          </div>
+        </div>
+        <div class="post-footer">
+          <div class="post-buttons">
+            <button class="post-button likes">
+              <svg width="19" height="20" class="icon icon-like">
+                <use xlink:href="img/icons.svg#like"></use>
+              </svg>
+              <span class="likes-counter">26</span>
+            </button>
+            <button class="post-button comments">
+              <svg width="21" height="21" class="icon icon-comment">
+                <use xlink:href="img/icons.svg#comment"></use>
+              </svg>
+              <span class="comments-counter">157</span>
+            </button>
+            <button class="post-button save">
+              <svg width="19" height="19" class="icon icon-save">
+                <use xlink:href="img/icons.svg#save"></use>
+              </svg>
+            </button>
+            <button class="post-button share">
+              <svg width="17" height="19" class="icon icon-share">
+                <use xlink:href="img/icons.svg#share"></use>
+              </svg>
+            </button>
+          </div>
+          <div class="post-author">
+            <div class="author-about">
+              <a href="#" class="author-username">arteislamov</a>
+              <span class="post-time">${date}</span>
+            </div>
+            <a href="#" class="author-link"><img src="img/avatar.jpeg" alt="avatar" class="author-avatar"></a>
+          </div>
+        </div>
+      </section>
+    `;
     
   })
   
-  postsWrapper.innerHTML = 'ТУТ МОГЛА БЫТЬ ВАША РЕКЛАМА!'
+  postsWrapper.innerHTML = postsHTML;
 };
 
 const init = () => {
